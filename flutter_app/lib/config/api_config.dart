97db53cbf4f242948e9d0f582435029537705d1d
Purab Sahare <purabsahare@gmail.com>
2026-01-10 12:21:00 +0530

    new-ip-address

--- flutter_app/lib/config/api_config.dart
+++ flutter_app/lib/config/api_config.dart
@@ -2,7 +2,7 @@
 /// Update baseUrl to switch between local and production environments
 class ApiConfig {
   // 💻 Local Development URL (Active) - Update this if your PC's IP changes
-  static const String baseUrl = 'http://10.167.232.25:8000';
+  static const String baseUrl = 'http://10.210.61.25:8000';
   
   // 🌐 Production URL (Azure) - Uncomment to use Azure backend
   // static const String baseUrl = 'https://yoga-app-rg-dchbg5dtg8aah2fw.centralindia-01.azurewebsites.net';
