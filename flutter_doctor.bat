2afbc14a1ed3de3fe83994bc7661d0520a29b577
Yuvraj Singh <3014yuvrajsingh@gmail.com>
2026-02-22 18:19:31 +0530

    Update flutter_doctor.bat

--- flutter_doctor.bat
+++ flutter_doctor.bat
@@ -4,6 +4,6 @@ set PATH=D:\COLLEGE\Tools\flutter\bin;D:\COLLEGE\Tools\Git\bin;%PATH%
 echo PATH set to: %PATH%
 echo.
 echo Running flutter doctor...
-flutter doctor
+flutter doctor --suppress-analytics
 echo.
 pause
